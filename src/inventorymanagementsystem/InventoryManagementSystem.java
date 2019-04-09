7b20b17f91100aafe8d5e186a8566272ef5ab2b3
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2019-04-09 10:32:01 +0600

    Update InventoryManagementSystem.java

--- src/inventorymanagementsystem/InventoryManagementSystem.java
+++ src/inventorymanagementsystem/InventoryManagementSystem.java
@@ -16,6 +16,7 @@ public class InventoryManagementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new Main_Window().setVisible(true);
     }
     
 }
